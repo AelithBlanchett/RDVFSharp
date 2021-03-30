285ae128f2d42042e3808f7c197895510a7ff925
Elise Pariat <tonyteur@yahoo.fr>
2021-03-30 18:07:49 -0400

    Added last balance and technical fixes

--- RDVFSharp/Commands/Ingame/Action.cs
+++ RDVFSharp/Commands/Ingame/Action.cs
@@ -14,6 +14,10 @@ namespace RDVFSharp.Commands
             {
                 Plugin.CurrentBattlefield.TakeAction(GetType().Name);
             }
+            else
+            {
+                Plugin.FChatClient.SendMessageInChannel("This is not your turn.", Plugin.Channel);
+            }
         }
     }
 }
--- RDVFSharp/Entities/Fighter.cs
+++ RDVFSharp/Entities/Fighter.cs
@@ -321,7 +321,7 @@ namespace RDVFSharp.Entities
             if (IsFocused > 0) Battlefield.WindowController.Hint.Add(Name + "'s Ranged and Spell attacks have a +" + Math.Ceiling((double)IsFocused / 10) + " bonus to attack and damage because of the Focus.");
             if (Battlefield.InGrabRange && Battlefield.DisplayGrabbed)
             {
-                Battlefield.WindowController.Hint.Add("The fighters are in grappling range"); //Added notification about fighters being in grappling range.
+                Battlefield.WindowController.Hint.Add("The fighters are in grappling range."); //Added notification about fighters being in grappling range.
             }
             Battlefield.DisplayGrabbed = !Battlefield.DisplayGrabbed; //only output it on every two turns
             return message;
@@ -509,6 +509,7 @@ namespace RDVFSharp.Entities
             if (attacker.IsRestrained) difficulty += 2; //Up the difficulty if the attacker is restrained.
             if (target.IsRestrained) difficulty -= 4; //Lower it if the target is restrained.
             if (target.IsExposed > 0) difficulty -= 2; // If opponent left themself wide open after a failed strong attack, they'll be easier to hit.
+
             if (target.IsEvading > 0)
             {//Evasion bonus from move/teleport. Only applies to one attack, then is reset to 0.
                 difficulty += target.IsEvading;
@@ -758,7 +759,8 @@ namespace RDVFSharp.Entities
         {
             var attacker = this;
             var target = Battlefield.GetTarget();
-            var damage = 0;
+            var damage = Utils.RollDice(new List<int>() { 6, 6 }) - 1 + attacker.Strength;
+            damage /= 2;
             var requiredStam = 10;
             var difficulty = 8; //Base difficulty, rolls greater than this amount will hit.
 
@@ -819,7 +821,6 @@ namespace RDVFSharp.Entities
 
             damage = Math.Max(damage, 0);
             if (damage > 0) target.HitHp(damage); //This is to prevent the game displayin that the attacker did 0 damage, which is the normal case.
-            attacker.IsAggressive = 5;
             target.IsStunned = true;
             return true; //Successful attack, if we ever need to check that.
         }
@@ -898,7 +899,7 @@ namespace RDVFSharp.Entities
                 }
                 else
                 {
-                    damage += 10;
+                    damage += Math.Max(0, 10 - target.IsEscaping);
                     Battlefield.WindowController.Hit.Add(attacker.Name + " THREW " + target.Name + " and dealt bonus damage!");
                 }
                 //Battlefield.WindowController.Hint.Add(target.Name + ", you are no longer grappled. You should make your post, but you should only emote being hit, do not try to perform any other actions.");
@@ -1013,7 +1014,7 @@ namespace RDVFSharp.Entities
             return true; //Successful attack, if we ever need to check that.
         }
 
-        public bool ActionMana(int roll)
+        public bool ActionMagic(int roll)
         {
             var attacker = this;
             var target = Battlefield.GetTarget();
@@ -1030,7 +1031,7 @@ namespace RDVFSharp.Entities
                 target.Fumbled = false;
             }
 
-            if (attacker.IsRestrained) difficulty += 2; //Math.Max(2, 4 + (int)Math.Floor((double)(target.Strength - attacker.Strength) / 2)); //When grappled, up the difficulty based on the relative strength of the combatants. Minimum of +2 difficulty, maximum of +8.
+            if (attacker.IsRestrained) difficulty += 2; //Math.max(2, 4 + Math.floor((target.strength() - attacker.strength()) / 2)); //When grappled, up the difficulty based on the relative strength of the combatants. Minimum of +2 difficulty, maximum of +8.
             if (target.IsRestrained) difficulty -= 4; //Lower the difficulty considerably if the target is restrained.
             if (target.IsExposed > 0) difficulty -= 2; // If opponent left themself wide open after a failed strong attack, they'll be easier to hit.
 
@@ -1101,6 +1102,94 @@ namespace RDVFSharp.Entities
             return true; //Successful attack, if we ever need to check that.
         }
 
+        public bool ActionMana(int roll)
+        {
+            var attacker = this;
+            var target = Battlefield.GetTarget();
+            var damage = Utils.RollDice(new List<int>() { 6, 6 }) - 1 + attacker.Spellpower;
+            damage *= 2;
+            damage += Math.Min(attacker.Strength, attacker.Spellpower);
+            var requiredMana = 10;
+            var difficulty = 8; //Base difficulty, rolls greater than this amount will hit.
+
+            //If opponent fumbled on their previous action they should become stunned.
+            if (target.Fumbled)
+            {
+                target.IsStunned = true;
+                target.Fumbled = false;
+            }
+
+            if (attacker.IsRestrained) difficulty += 2; //Math.Max(2, 4 + (int)Math.Floor((double)(target.Strength - attacker.Strength) / 2)); //When grappled, up the difficulty based on the relative strength of the combatants. Minimum of +2 difficulty, maximum of +8.
+            if (target.IsRestrained) difficulty -= 4; //Lower the difficulty considerably if the target is restrained.
+            if (target.IsExposed > 0) difficulty -= 2; // If opponent left themself wide open after a failed strong attack, they'll be easier to hit.
+
+            if (target.IsEvading > 0)
+            {//Evasion bonus from move/teleport. Only applies to one attack, then is reset to 0.
+                difficulty += target.IsEvading;
+                damage -= target.IsEvading;
+                target.IsEvading = 0;
+            }
+            if (attacker.IsAggressive > 0)
+            {//Apply attack bonus from move/teleport then reset it.
+                difficulty -= attacker.IsAggressive;
+                damage += attacker.IsAggressive;
+                attacker.IsAggressive = 0;
+            }
+
+            var critCheck = true;
+            if (attacker.Mana < requiredMana)
+            {   //Not enough mana-- reduced effect
+                critCheck = false;
+                damage *= attacker.Mana / requiredMana;
+                difficulty += (int)Math.Ceiling((double)((requiredMana - attacker.Mana) / requiredMana) * (20 - difficulty)); // Too tired? You're likely to have your spell fizzle.
+                Battlefield.WindowController.Hint.Add(attacker.Name + " did not have enough mana, and took penalties to the attack.");
+            }
+
+            attacker.HitMana(requiredMana); //Now that required mana has been checked, reduce the attacker's mana by the appopriate amount.
+
+            var attackTable = attacker.BuildActionTable(difficulty, target.Dexterity, attacker.Dexterity, target.Mana, target.ManaCap);
+            //If target can dodge the atatcker has to roll higher than the dodge value. Otherwise they need to roll higher than the miss value. We display the relevant value in the output.
+            Battlefield.WindowController.Info.Add("Dice Roll Required: " + (attackTable.miss + 1));
+
+            if (roll <= attackTable.miss)
+            {   //Miss-- no effect.
+                Battlefield.WindowController.Hit.Add(" FAILED!");
+                attacker.IsExposed += 2; //If the fighter misses a big attack, it leaves them open and they have to recover balance which gives the opponent a chance to strike.
+                Battlefield.WindowController.Hint.Add(attacker.Name + " was left wide open by the failed attack and is now Exposed! " + target.Name + " has -2 difficulty to hit and can use Grab even if fighters are not in grappling range!");
+                return false; //Failed attack, if we ever need to check that.
+            }
+
+            if (roll >= attackTable.crit)
+            { //Critical Hit-- increased damage/effect, typically 3x damage if there are no other bonuses.
+                Battlefield.WindowController.Hit.Add(" CRITICAL HIT! ");
+                Battlefield.WindowController.Hint.Add(attacker.Name + " landed a particularly vicious blow!");
+                damage += 10;
+                Battlefield.WindowController.Hint.Add("Critical Hit! " + attacker.Name + "'s magic worked abnormally well! " + target.Name + " is dazed and disoriented.");
+            }
+            else
+            { //Normal hit.
+                Battlefield.WindowController.Hit.Add("MAGIC HIT! ");
+            }
+
+            //Deal all the actual damage/effects here.
+
+            if (Battlefield.InGrabRange)
+            {// Succesful attacks will beat back the grabber before they can grab you, but not if you're already grappling.
+                if (!attacker.IsRestrained && !target.IsRestrained)
+                {
+                    Battlefield.InGrabRange = false;
+                    Battlefield.WindowController.Hit.Add(attacker.Name + " distracted " + target.Name + " with the attack and was able to move out of grappling range!");
+                }
+            }
+
+            //If you're being grappled and you hit the opponent that will make it a little easier to escape later on.
+            if (attacker.IsRestrained) attacker.IsEscaping += (int)Math.Floor((double)damage / 5);
+
+            damage = Math.Max(damage, 1);
+            target.HitHp(damage);
+            return true; //Successful attack, if we ever need to check that.
+        }
+
 
         public bool ActionHex(int roll)
         {
@@ -1119,7 +1208,8 @@ namespace RDVFSharp.Entities
 
             if (attacker.IsRestrained) difficulty += 2;
             if (target.IsRestrained) difficulty -= 4; //Ranged attacks during grapple are hard, but Hex is now melee.
-                                                      //if (attacker.isFocused) difficulty -= 4; //Lower the difficulty if the attacker is focused
+            if (target.IsExposed > 0) difficulty -= 2; // If opponent left themself wide open after a failed strong attack, they'll be easier to hit.
+
 
             if (target.IsEvading > 0)
             {//Evasion bonus from move/teleport. Only applies to one attack, then is reset to 0.
@@ -1648,7 +1738,7 @@ namespace RDVFSharp.Entities
             if (!target.Fumbled)
             {
                 attacker.Fumbled = true;
-                Battlefield.WindowController.Hint.Add(attacker.Name + " loses the next action!");
+                Battlefield.WindowController.Hint.Add(attacker.Name + " loses the next action and is Exposed!");
             }
             else
             {
