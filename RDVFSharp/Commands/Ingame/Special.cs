59b7855b319525888e3c06472cda74bb675219ad
monknike <85686946+monknike@users.noreply.github.com>
2021-10-09 13:34:49 +0100

    Update Special.cs

--- RDVFSharp/Commands/Ingame/Special.cs
+++ RDVFSharp/Commands/Ingame/Special.cs
@@ -1,1 +1,18 @@
-Hi
+
+namespace RDVFSharp.Commands
+{
+    public class Tackle : Action
+    {
+        public override void ExecuteCommand(string character, IEnumerable<string> args, string channel)
+        {
+            if (!Plugin.CurrentBattlefield.InGrabRange)
+            {
+                base.ExecuteCommand(character, args, channel);
+            }
+            else
+            {
+                Plugin.FChatClient.SendMessageInChannel("You have already used special once in this match!", Plugin.Channel);
+            }
+        }
+    }
+}
