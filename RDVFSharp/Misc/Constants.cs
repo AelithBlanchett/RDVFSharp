1e98cd742640221ebeb7f09f00ce7db6a5ff0763
monknike <85686946+monknike@users.noreply.github.com>
2024-05-20 18:02:44 +0100

    Made help command easier to edit, and updated it

--- RDVFSharp/Misc/Constants.cs
+++ RDVFSharp/Misc/Constants.cs
@@ -30,45 +30,57 @@ Conditions: +18 Good human(-oids) profiles only [sub]no furries, sorry[/sub]
 [color=red][sub]☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰☰[/sub][/color]";
 
 
-        public const string Help = "[u][b]IF YOU WANT TO SEE A COMPLETE DETAILED DESCRIPTION OF THESE COMMANDS, PLEASE LOOK AT THE ACTION DETAILS LIST HERE:[/b][/u] [icon]Rendezvous Fight[/icon]" + "\n" +
-                    "[u][b]General Commands[/b][/u]" + "\n" +
-                    "\n" +
-                    "[b]!ready[/b]: Inserts your character into the battlefield. Preferably followed by one of four colours (red/blue/yellow/purple). And also preferably done in such a way that each team member readies right after each other, so that the status looks better. Example: !ready red. Not usable in the bar!" + "\n" +
-                    "[b]!start[/b]: Starts the match! (PS: If you type the name of an arena after !start, then you choose the arena! E.G. typing '!start COLISEUM' will name the arena 'COLISEUM'!" + "\n" +
-                    "[b]!register[/b]: Registers you with the fight bot. Must be followed by your STR, DEX, RES, SPW, and WIL values, in that order. Example: !register 5 8 8 1 2" + "\n" +
-                    "[b]!restat[/b]: Changes your stats on the fight bot. Must be followed by your STR, DEX, RES, SPW, and WIL values, in that order. Example: !restat 5 8 8 1 2" + "\n" +
-                    "[b]!stats[/b]: The bot will PM you with your stats." + "\n" +
-                    "[b]!forfeit[/b]: Ends the match, and declares your opponent the winner." + "\n" +
-                    "[b]!status[/b]: Shows you the last message sent by the bot in the fight. If the bot does not send a status message in response to your command, or to check if a fight is occurring in the venue/arena, please use this! Now also comes with a '!status Arena'/'!status Venue' command which can be used in the bar to see these same messages, or just use '!status' in the bar to see if a fight is happening in any of the fight rooms!" + "\n" +
-                    "[b]!leave[/b]: If you've readied, but there is no match going on, this clears the arena. Otherwise, if all the participants in an ongoing match type this, the fight becomes null and void." + "\n" +
-                    "[b]!help/!commands[/b]: Makes the bot PM all the commands available to you." + "\n" +
-                    "[b]!createroom[/b]: Makes a new private room for you to invite people to! Invite them using the '/invite name' command in the room, and it will come up on their console. Only usable in the bar. Also, remember to add the name of the room after the command! Example: !createroom Mayank vs Silver. Otherwise, the room will just be named 'RDVF - '" + "\n" +
-                    "[b]!looking[/b]: Add yourself to a list of people looking for a fight! This command expires in 2 hours, though it will let you know when it disappears so you can reset the timer by doing the command again! Only usable in the bar" + "\n" +
-                    "[b]!look[/b]: Look at the list of people that are looking for a fight! Only usable in the bar" + "\n" +
-                    "\n" +
-                    "\n" +
-                    "[u][b]Infight commands[/b][/u]" + "\n" +
-                    "[b]!target[/b]: Changes your target. Must be followed by the name of the person you wish to target. Example: !target Mayank. THIS DOES NOT USE YOUR TURN UP. Please, always double check your targets to make sure they are correct!" + "\n" +
-                    "[b]!light[/b]: A medium damage STR based attack that does damage to both HP and stamina." + "\n" +
-                    "[b]!heavy[/b]: A high damage STR based attack that does damage to HP. Leaves you exposed on a miss." + "\n" +
-                    "[b]!ranged[/b]: A high damage STR and DEX based attack that does damage to HP. Harder to hit than a !heavy, but does not leave you exposed." + "\n" +
-                    "[b]!cleave[/b]: A high damage STR based attack that does damage to your target's entire team. Only usable in fights with more than 2 fighters, but only recommended for use in team fights!" + "\n" +
-                    "[b]!tackle[/b]: A low damage STR based attack that stuns your opponents on hit, giving you another turn, and moves you into grappling range." + "\n" +
-                    "[b]!grab[/b]: A low damage STR based attack that grapples your opponent." + "\n" +
-                    "[b]!throw[/b]: A high damage STR based attack that can only be used while grappling your opponent. Frees you from being grabbed, and does extra damage if you're not being grabbed. Ends the 'grabbed' status." + "\n" +
-                    "[b]!submission[/b]: A fixed damage finisher (30 damage) that can only be used when grappling." + "\n" +
-                    "[b]!stab[/b]: A DEX based damage over time move that does damage to both HP and stamina." + "\n" +
-                    "[b]!move[/b]: A DEX based move that provides a one time attack buff when not grabbed, or a defensive buff when escaping from a grapple." + "\n" +
-                    "[b]!hex[/b]: A medium damage SPW based attack that does damage to both HP and mana." + "\n" +
-                    "[b]!magic[/b]: A high damage SPW based attack that does damage to HP. Leaves you exposed on a miss." + "\n" +
-                    "[b]!spell[/b]: A high damage SPW based attack that does damage to HP. Harder to hit than a !heavy, but does not leave you exposed." + "\n" +
-                    "[b]!manastorm[/b]: A high damage SPW based attack that does damage to your target's entire team. Does double the damage to the target than the rest of the team. Only usable in fights with more than 2 fighters, but only recommended for use in team fights!" + "\n" +
-                    "[b]!curse[/b]: A single use (Even if it misses, it counts!) maximum damage magic attack. Note: Do not use this move if you have too many points in STR, as they are subtracted from the overall damage, rather than added to it!" + "\n" +
-                    "[b]!drain[/b]: An SPW based damage over time move that does damage to both HP and mana." + "\n" +
-                    "[b]!teleport[/b]: An SPW based move that provides a one time attack buff when not grabbed, or a defensive buff when escaping from a grapple." + "\n" +
-                    "[b]!rest[/b]: A WIL based move that regains stamina." + "\n" +
-                    "[b]!channel[/b]: A WIL based move that regains mana." + "\n" +
-                    "[b]!focus[/b]: A WIL based move that adds damage to ranged or spell attacks. Lowers as you take damage from your opponent: And can be stacked." + "\n" +
-                    "[b]!guard[/b]: A WIL based move that provides a defensive buff to your character. Only usable in fights with more than 2 fighters!";
+        public const string Help = @"
+[u][b]IF YOU WANT TO SEE A COMPLETE DETAILED DESCRIPTION OF THESE COMMANDS, PLEASE LOOK AT THE ACTION DETAILS LIST HERE:[/b][/u] [icon]Rendezvous Fight[/icon]
+
+[u][b]General Commands[/b][/u]
+[b]!ready[/b]: Inserts your character into the battlefield. Preferably followed by one of four colours (red/blue/yellow/purple). And also preferably done in such a way that each team member readies right after each other, so that the status looks better. Example: !ready red. Not usable in the bar!
+[b]!start[/b]: Starts the match! (PS: If you type the name of an arena after !start, then you choose the arena! E.G. typing '!start COLISEUM' will name the arena 'COLISEUM'!
+[b]!register[/b]: Registers you with the fight bot. Must be followed by your STR, DEX, RES, SPW, and WIL values, in that order. Example: !register 5 8 8 1 2
+[b]!restat[/b]: Changes your stats on the fight bot. Must be followed by your STR, DEX, RES, SPW, and WIL values, in that order. Example: !restat 5 8 8 1 2
+[b]!stats[/b]: The bot will PM you with your stats.
+[b]!forfeit[/b]: Ends the match, and declares your opponent the winner.
+[b]!status[/b]: Shows you the last message sent by the bot in the fight. If the bot does not send a status message in response to your command, or to check if a fight is occurring in the venue/arena, please use this! Now also comes with a '!status Arena'/'!status Venue' command which can be used in the bar to see these same messages, or just use '!status' in the bar to see if a fight is happening in any of the fight rooms!
+[b]!leave[/b]: If you've readied, but there is no match going on, this clears the arena. Otherwise, if all the participants in an ongoing match type this, the fight becomes null and void.
+[b]!help/!commands[/b]: Makes the bot PM all the commands available to you.
+[b]!createroom[/b]: Makes a new private room for you to invite people to! Invite them using the '/invite name' command in the room, and it will come up on their console. Only usable in the bar. Also, remember to add the name of the room after the command! Example: !createroom Mayank vs Silver. Otherwise, the room will just be named 'RDVF - '
+[b]!looking[/b]: Add yourself to a list of people looking for a fight! This command expires in 2 hours, though it will let you know when it disappears so you can reset the timer by doing the command again! Only usable in the bar
+[b]!look[/b]: Look at the list of people that are looking for a fight! Only usable in the bar
+
+
+[u][b]Infight commands[/b][/u]
+[u][b]Physical Attacks[/b][/u]
+[b]!light[/b]: A medium damage STR based attack that does damage to both HP and stamina.
+[b]!heavy[/b]: A high damage STR based attack that does damage to HP. Leaves you exposed on a miss.
+[b]!ranged[/b]: A high damage STR and DEX based attack that does damage to HP. Harder to hit than a !heavy, but does not leave you exposed on a miss.
+[b]!tackle[/b]: A low damage STR based attack that stuns your opponents on hit, giving you another turn, and moves you into grappling range. Leaves you exposed on a miss.
+[b]!grab[/b]: A low damage STR based attack that grapples your opponent.
+[b]!throw[/b]: A high damage STR based attack that ends the 'grabbed' status for both you and the opponent, and does bonus damage if your opponent is not grabbing you, but you’re grabbing them.
+[b]!submission[/b]: A fixed damage finisher (30 damage) that can only be used when grappling. If grappled whilst using it: Forces your opponent to release you. Note: This is a high stamina cost move (25 stamina), and the difficulty is also affected by STR, not just DEX like every other move. 
+[b]!stab[/b]: A DEX based damage over time move that does damage to both HP and stamina when the opponent takes an action.
+
+[u][b]Magic Attacks[/b][/u]
+[b]!hex[/b]: A medium damage SPW based attack that does damage to both HP and mana.
+[b]!magic[/b]: A high damage SPW based attack that does damage to HP. Leaves you exposed on a miss.
+[b]!spell[/b]: A high damage SPW based attack that does damage to HP. Harder to hit than a !magic, but does not leave you exposed on a miss.
+[b]!curse[/b]: A single use (Even if it misses, it counts!) maximum damage spell attack. Note: Do not use this move if you have too many points in STR, as they are subtracted from the overall damage, rather than added to it!
+[b]!drain[/b]: An SPW based damage over time move that does damage to both HP and mana when the opponent takes an action.
+
+[u][b](De)Buffs/Recovery[/b][/u]
+[b]!move[/b]: A DEX based move that provides a one time attack buff when not grabbed, or a defensive buff when escaping from a grapple.
+[b]!teleport[/b]: An SPW based move that provides a one time attack buff when not grabbed, or a defensive buff when escaping from a grapple.
+[b]!rest[/b]: A WIL based move that regains stamina.
+[b]!channel[/b]: A WIL based move that regains mana.
+[b]!focus[/b]: A WIL based move that adds damage to ranged or spell attacks. Lowers as you take damage from your opponent: And can be stacked.
+[b]!skip[/b]: Skips your current turn.
+[b]!fumble[/b]: Skips your current turn, and leaves you exposed.
+[b]!forfeit[/b]: Immediately reduces your HP to 0.
+
+[u][b]Team/Free For All specific commands[/b][/u]
+[b]!target[/b]: Changes your target. Must be followed by the name of the person you wish to target. Example: !target Mayank. THIS DOES NOT USE YOUR TURN UP. Please, always double check your targets to make sure they are correct!
+[b]!guard[/b]: A WIL based move that provides a defensive buff to your character. Only usable in fights with more than 2 fighters!
+[b]!cleave[/b]: A high damage STR based attack that does damage to your target's entire team. Only usable in fights with more than 2 fighters, but only recommended for use in team fights!
+[b]!manastorm[/b]: A high damage SPW based attack that does damage to your target's entire team. Does double the damage to the target than the rest of the team. Only usable in fights with more than 2 fighters, but only recommended for use in team fights!
+";
     }
 }
